504959934eb7ea25437e10d462db14d6568c992e
Mario Jäckle <mario.j@jacksbox.de>
2018-05-17 16:59:49 +0200

    updated code a bit

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-document.getElementById('globalContainer').style.display = 'none';
-var customOnClick = "document.getElementById('globalContainer').style.display = 'block'; document.getElementById('myCustomOvelay').style.display = 'none';";
-var cHtml = '<div id="myCustomOvelay" style="position: absolute; top: 50%; left: 50%; transorm: translate(50%, 50%); text-align: center;">';
-    cHtml += '<h1 style="font-size: 22px">';
-    cHtml += 'Are u serious?<br>';
-    cHtml += '</h1>';
-    cHtml += '<span onClick="'+customOnClick+'" style="cursor:pointer">hm, ok</span>';
-    cHtml += '</div>';
-document.getElementsByTagName('body')[0].innerHTML += cHtml;
+(function(){
+  document.getElementById('globalContainer').style.opacity = 0;
+  var cHtml  = '<div id="jbOverlay" style="position: absolute; top: 50%; left: 50%; transorm: translate(50%, 50%); text-align: center;">';
+      cHtml += '<h1 style="font-size: 22px">';
+      cHtml += 'Are u serious?<br>';
+      cHtml += '</h1>';
+      cHtml += '<button id="jbButton">hm, ok</button>';
+      cHtml += '</div>';
+  document.getElementsByTagName('body')[0].innerHTML += cHtml;
+  document.getElementById('jbButton').addEventListener('click', function() {
+    document.getElementById('globalContainer').style.opacity = 1;
+    var overlay = document.getElementById('jbOverlay');
+        overlay.parentElement.removeChild(overlay);
+  })
+})()
